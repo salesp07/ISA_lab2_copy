2b221c05bdd4fbb7bee7d7501d7896e14087cfe6
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-01-30 19:21:49 -0800

    initial commit

---
+++ public/client.js
@@ -0,0 +1,40 @@
+// function getNumberInfo(number) {
+// 	const API_URL = `http://numbersapi.com/${number}`;
+// 	fetch(API_URL).then(response => response.text()).then(data => {
+// 		console.log(data);
+// 	}).catch(error => {
+// 		console.log(error);
+// 	});
+// }
+// getNumberInfo(12)
+
+
+$(document).ready(function() {
+	$('#chatbot-form').submit(function(event) {
+		event.preventDefault();
+		const message = $('#chatbot-input').val();
+		$.ajax({
+			type: 'POST',
+			url: 'https://isa-lab2.onrender.com/chatbot',
+			data: {
+				message: message
+			},
+			success: function(response) {
+				$('#chatbot-response').text(response.text);
+				let newMessage = $('<div>', {
+					class: 'message'
+				}).text(response.text);
+				let removeButton = $('<button>', {
+					class: 'remove-button'
+				}).text('Remove');
+				newMessage.append(removeButton);
+				$('#chat-history').append(newMessage);
+			}
+		});
+	});
+
+	$(document).on('click', '.remove-button', function() {
+		$(this).parent().remove();
+	});
+});
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const app = express()
+app.use(express.json()); // read JSON BODY
+app.use(express.urlencoded({ extended: true })); // read URL encoded body
+// app.use(express.static(__dirname + '/public'));
+
+
+app.post('/chatbot', (req, res) => {
+	const message = req.body.message;
+	const number = message.match(/\d+/);
+	if (number) {
+		axios.get(`http://numbersapi.com/${number}?type=trivia`).then(response => {
+			res.json({
+				text: response.data
+			});
+		}).catch(error => {
+			res.json({
+				text: "Sorry, I couldn't find any information about that number."
+			});
+		});
+	} else {
+		res.json({
+			text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
+		});
+	}
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
